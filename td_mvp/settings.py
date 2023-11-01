e8ac705cdf412aa7bb1645e0f8128dfac4d25348
Gosha Kokorin <go_2002@mail.ru>
2023-11-01 18:20:57 +0500

    add: project

--- td_mvp/settings.py
+++ td_mvp/settings.py
@@ -128,14 +128,13 @@ SPECTACULAR_SETTINGS = {
 
 
 CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost",
+#     "http://localhost:8000",
+#     "http://127.0.0.1:8000",
+#     "http://127.0.0.1",
+#     "http://localhost:3000",
+#     "http://localhost:5779",
+# ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
-    "http://127.0.0.1",
-    "http://localhost:3000",
-    "http://localhost:5779",
-]
-
-CSRF_TRUSTED_ORIGINS = ['http://api.lasermig.ru', 'https://lasermig.ru']
+CSRF_TRUSTED_ORIGINS = ['https://tdmvp.ru',]
